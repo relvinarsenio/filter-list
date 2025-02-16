f71606efbb011f8e14097424dd92e251801e709d
Relvin Arsenio <88470966+relvinarsenio@users.noreply.github.com>
2025-02-16 16:04:59 +0700

    Update

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,6 +3,6 @@
 # Exit immediately if a command exits with a non-zero status
 set -e
 
-# Compiling AdGuard DNS filter
+# Compiling Enhanced Security DNS filter
 hostlist-compiler -c configuration.json -o Filters/filter.txt --verbose
 node scripts/filter_build.js Filters/filter.txt

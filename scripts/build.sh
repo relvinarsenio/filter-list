9a9d31a764b21d8a62e2debde3549685c3337da3
Relvin Arsenio <88470966+relvinarsenio@users.noreply.github.com>
2025-03-25 20:12:43 +0700

    Add New Configuration (Test)

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,4 +5,6 @@ set -e
 
 # Compiling Enhanced Security DNS filter
 hostlist-compiler -c configuration.json -o Filters/filter.txt --verbose
+hostlist-compiler -c configuration-orion.json -o Filters/filter-orion.txt --verbose
 node scripts/filter_build.js Filters/filter.txt
+node scripts/filter_build.js Filters/filter-orion.txt
